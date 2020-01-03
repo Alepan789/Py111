f04beef6d44b469c60cc5c57113de8067a17c679
alepan789 <alpan82@list.ru>
2020-01-03 03:03:34 +0300

    решение через ряды - вариант2

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -1,62 +1,78 @@
 """
-Taylor series
+Taylor series - вариант 2
 """
+
 from typing import Union
 
-
-def ex(x: Union[int, float]) -> float:
+def ex(x: Union[int, float], accur: float = 0.0000001) -> float:
     """
     Calculate value of e^x with Taylor series
     # 1 + x/1! + x**2/2! + x**3/3! + ...
     :param x: x value
+    :param accur: точность вычисления
     :return: e^x value
     """
 
-    def xfact(x):
-        i = 1
-        rez = 1
-        while i <= x:
-            rez *= i
-            i += 1
-        return rez
-
-    diff = 0.0000001  # условие прекращения вычисления ряда
     i = 2
-    prev_step = x / xfact(1)
-    next_step = x ** 2 / xfact(2)
+    #делитель 1!
+    prev_step = x / 1
+
+    #делимое
+    top_part = x ** 2
+
+    #делитель == 2!
+    btm_part = 2
+
+    next_step = top_part / btm_part
     rez = 1 + prev_step + next_step
     # print(f'i:{i}\t\tprev:{prev_step}\tnext:{next_step}\tdelta{next_step - prev_step}\tfact:{xfact(i)}')
-    while abs(prev_step - next_step) > diff or next_step > diff:
+    while abs(prev_step - next_step) > accur or next_step > accur:
         i += 1
         prev_step = next_step
-        next_step = (x ** i) / xfact(i)
+        top_part *= x
+        btm_part *= i
+        next_step = top_part / btm_part
         rez += next_step
         # print(f'i:{i}\tprev:{prev_step}\tnext:{next_step}\tdelta{prev_step - next_step}\tfact:{xfact(i)} \tx**i:{x**i} ')
     return rez
 
 
-def sinx(x: Union[int, float]) -> float:
+def sinx(x: Union[int, float], accur: float = 0.0000001) -> float:
     """
     Calculate sin(x) with Taylor series
+    # x - x**3/3! + x**5/5! - x**7/7! + x**9/9! ...
     :param x: x value
+    :param accur: точность вычисления следующего шага
     :return: sin(x) value
     """
 
-    diff = 0.0000001  # условие прекращения вычисления ряда
     i = 2
-    xfactorial = 1 * 2 * 3
-    prev_step = x ** 3 / xfactorial
-    xfactorial = xfactorial * 4 * 5
-    next_step = x ** 5 / xfactorial
+    top_part = x ** 3
+    btm_part = 1 * 2 * 3
+    prev_step = top_part / btm_part
+    top_part = top_part * x * x
+    btm_part = btm_part * 4 * 5
+    next_step = top_part / btm_part
     rez = x - prev_step + next_step
     # print(f'i:{i}\t\tprev:{prev_step}\tnext:{next_step}\tdelta{next_step - prev_step}\tfact:{xfactorial}')
-    while abs(prev_step - next_step) > diff or next_step > diff:
+
+    while abs(prev_step - next_step) > accur or next_step > accur:
         i += 1
-        # ii = i*2+1
-        xfactorial = xfactorial * (i * 2) * (i * 2 + 1)
+        if i % 2 == 0:
+            prefix = 1
+        else:
+            prefix = -1
         prev_step = next_step
-        next_step = ((-1) ** i) * (x ** (2 * i + 1) / xfactorial)
+        top_part = top_part * x * x
+        btm_part = btm_part * (i * 2) * (i * 2 + 1)
+        next_step = prefix * (top_part / btm_part)
         rez += next_step
-        # print(f'i:{i}\tprev:{prev_step}\tnext:{next_step}\tdelta{prev_step - next_step}\tfact:{xfactorial} \tii:{ii} ')
+
+        # print(f'i:{i}\tprev:{prev_step}\tnext:{next_step}\tdelta{prev_step - next_step}\tfact:{xfactorial} \tprefix:{prefix} ')
 
     return rez
+
+# import math
+# print(ex(2))
+# print(sinx(22))
+# print(math.sin(22))
--- Tests/b2_test_Taylor.py
+++ Tests/b2_test_Taylor.py
@@ -8,9 +8,17 @@ class MyTestCase(unittest.TestCase):
 		const = 1.55433
 		self.assertAlmostEqual(math.exp(const), ex(const), delta=0.0001, msg="More accurate! :D")
 
+	def test2_ex(self):
+		const = 1.55433
+		self.assertAlmostEqual(math.exp(const), ex(const, 0.0001), delta=0.000001, msg="More accurate part2! :D")
+
 	def test_sinx(self):
 		const = 1.55433
 		self.assertAlmostEqual(math.sin(const), sinx(const), delta=0.0001, msg="More accurate! :D")
+
+	def test2_sinx(self):
+		const = 1.55433
+		self.assertAlmostEqual(math.sin(const), sinx(const, 0.0001), delta=0.00000001, msg="More accurate! :D")
 
 
 if __name__ == '__main__':
