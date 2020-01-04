5a77a255fb254a35245f878bbe67bed60d53a303
alepan789 <alpan82@list.ru>
2020-01-05 00:17:26 +0300

    a0 - стек решение через ряды - вариант2

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -3,42 +3,48 @@ My little Stack
 """
 from typing import Any
 
+stack = []
+
 
 def push(elem: Any) -> None:
-	"""
-	Operation that add element to stack
+    """
+       Operation that add element to stack
 
-	:param elem: element to be pushed
-	:return: Nothing
-	"""
-	print(elem)
-	return None
+       :param elem: element to be pushed
+       :return: Nothing
+    """
+    stack.append(elem)
+    # print(elem)
+    return None
 
 
 def pop() -> Any:
-	"""
-	Pop element from the top of the stack. If not elements - should return None.
-
-	:return: popped element
-	"""
-	return None
+    """
+        Pop element from the top of the stack. If not elements - should return None.
+        :return: popped element
+    """
+    if stack:
+        return stack.pop()
+    return None
 
 
 def peek(ind: int = 0) -> Any:
-	"""
-	Allow you to see at the element in the stack without popping it.
+    """
+          Allow you to see at the element in the stack without popping it.
 
-	:param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
-	:return: peeked element or None if no element in this place
-	"""
-	print(ind)
-	return None
+       :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
+       :return: peeked element or None if no element in this place	"""
+    if len(stack) > ind + 1:
+        return stack[-(ind + 1)]
+    # print(ind)
+    return None
 
 
 def clear() -> None:
-	"""
-	Clear my stack
+    """
+           Clear my stack
 
-	:return: None
-	"""
-	return None
+       :return: None
+    """
+    stack.clear()
+    return None
