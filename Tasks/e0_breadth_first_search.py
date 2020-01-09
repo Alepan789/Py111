e354ce74d0c2c79243ba5307eea5945555643513
alepan789 <alpan82@list.ru>
2020-01-09 21:22:48 +0300

    BFS graph

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,5 +1,6 @@
 from typing import Hashable, List
 import networkx as nx
+import matplotlib.pyplot as plt
 
 
 def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
@@ -10,5 +11,51 @@ def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
 	:param start_node: starting node for search
 	:return: list of nodes in the visited order
 	"""
-	print(g, start_node)
+	vrez = [start_node]
+	vnext = []
+	i = 0
+	while True and i < 10:
+		for n in g.neighbors(start_node):
+			if n not in vnext:
+				vnext.append(n)
+				start_node = n
+			# elif n in vrez and n in  :
+			#
+		print(f'i:{i} n:{n}')
+		i += 1
+	print(vnext)
+	print(list(g.nodes))
 	return list(g.nodes)
+
+
+graph = nx.Graph()
+# graph.add_nodes_from("ABCDEFGHIJ")
+# graph.add_edges_from([
+# 	('A', 'B'),
+# 	('A', 'F'),
+# 	('B', 'G'),
+# 	('F', 'G'),
+# 	('G', 'C'),
+# 	('G', 'H'),
+# 	('G', 'I'),
+# 	('C', 'H'),
+# 	('I', 'H'),
+# 	('H', 'D'),
+# 	('H', 'E'),
+# 	('H', 'J'),
+# 	('E', 'D'),
+# ])
+
+graph.add_nodes_from("ABCD")
+graph.add_edges_from([
+	('A', 'B'),
+	('B', 'C'),
+	('B', 'D'),
+
+])
+
+plt.subplot(121)
+nx.draw(graph, with_labels=True, font_weight='bold')
+plt.show()
+
+bfs(graph, 'A')
