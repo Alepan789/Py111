d14350e7654aba2e09b00bdcf56e420b3e66fce9
alepan789 <alpan82@list.ru>
2020-01-09 06:32:52 +0300

    решение через ряды - вариант2

---
+++ Tasks/00_Test_ForPY111.py
@@ -0,0 +1,7 @@
+"""
+проверка работы ГИТ и тд и тп
+"""
+biblio = []
+biblio.append({"name":"Война и Мир", "autor":"Л.Н.Толстой"})
+biblio.append({"name":"Евгений Онегин", "autor":"А.С.Пушкин"})
+print(biblio)
