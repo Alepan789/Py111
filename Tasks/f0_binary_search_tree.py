344fb839426293734b220b667a08336ae8fbb04c
alepan789 <alpan82@list.ru>
2020-01-11 13:18:52 +0300

    BFS graph

--- Tasks/f0_binary_search_tree.py
+++ Tasks/f0_binary_search_tree.py
@@ -4,7 +4,10 @@ or with dicts (smth like {'key': 0, value: 123, 'left': {...}, 'right':{...}})
 """
 
 from typing import Hashable, Any, Optional, Tuple
-# import networkx as nx
+import networkx as nx
+
+_root: Optional[Hashable] = None
+_tree = nx.Graph()
 
 
 def insert(key: Hashable, value: Any) -> None:
@@ -15,7 +18,17 @@ def insert(key: Hashable, value: Any) -> None:
 	:param value: value associated with key
 	:return: None
 	"""
-	print(key, value)
+	global _root
+	if _root is None:
+		_root = key
+		_tree.add_node(key, value=value)
+	else:
+		_value, root_node = _find_in_subTree(key, _root, None)
+
+		if root_node != key:
+			_tree.add_edge(root_node, key)
+	_tree.add_node(key, value=value)
+
 	return None
 
 
@@ -26,8 +39,21 @@ def remove(key: Hashable) -> Optional[Tuple[Hashable, Any]]:
 	:param key: key to be removed
 	:return: deleted (key, value) pair or None
 	"""
-	print(key)
-	return None
+	_tree.remove_node(key)
+	print(f'remove:{key}  tree:{list(_tree.nodes)}')
+	# return None
+
+def _find_in_subTree(key: Hashable, root: Hashable, parent) -> Tuple[Any, Hashable]:
+	if key == root:
+		return _tree.nodes[root]["value"], root
+
+	for node in _tree.neighbors(root):
+		if node == parent:
+			continue
+		if node < root and node < root or \
+				node > root and node > root:
+			return _find_in_subTree(key, node, root)
+	return None, root
 
 
 def find(key: Hashable) -> Optional[Any]:
@@ -37,7 +63,11 @@ def find(key: Hashable) -> Optional[Any]:
 	:param key: key for search in the BST
 	:return: value associated with the corresponding key
 	"""
-	print(key)
+	global _root
+	if _root is None:
+		_root = key
+
+	# 	print(key)
 	return None
 
 
@@ -47,4 +77,44 @@ def clear() -> None:
 
 	:return: None
 	"""
-	return None
+	global _root
+	_root = None
+	_tree.clear()
+
+# return None
+
+#
+#
+# _tree.add_nodes_from('abcd')
+# print(list(_tree.nodes))
+#
+# remove('b')
+# # clear()
+#
+# print(list(_tree.nodes))
+
+# _root = 6
+# _tree.add_nodes_from('134569')
+# _tree.add_edges_from([
+# 	(3, 5),
+# 	(3, 1),
+# 	(3, 4),
+# 	(9, 5),
+# 	(9, 6)
+# ])
+
+
+# print
+# _root = 5
+
+insert('5', 5)
+insert('3', 3)
+insert('9', 9)
+insert('1', 1)
+insert('4', 4)
+insert('6', 6)
+insert('7', 7)
+insert('7', 9)
+
+
+print(list(_tree.nodes))
--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -10,4 +10,96 @@ def sort(container: Collection[_Tt]) -> Collection[_Tt]:
 	:param container: container of elements to be sorted
 	:return: container sorted in ascending order
 	"""
-	return container
+	j = 1
+	new_container = list(container)
+	no_change = False
+	while not no_change:
+		no_change = True
+		i = 0
+		while i < len(container) - 1:
+			if new_container[i + 1] < new_container[i]:
+				new_container[i], new_container[i + 1] = new_container[i + 1], new_container[i]
+				no_change = False
+			i += 1
+			# print(f'j:{j} i:{i} rez:{no_change} Collection:{new_container}')
+		print(f'j:{j} i:{i} rez:{no_change} Collection:{new_container}')
+		j += 1
+	print(f'\nResult: {new_container} type:{type(container)}\n')
+	return new_container
+
+
+
+def sort2(container: Collection[_Tt]) -> Collection[_Tt]:
+	"""
+	Sort input container with bubble sort
+
+	:param container: container of elements to be sorted
+	:return: container sorted in ascending order
+	"""
+	step = 1
+	new_container = list(container)
+	no_change = False
+	while not no_change:
+		no_change = True
+		for i in range(len(container) - 1):
+			if new_container[i + 1] < new_container[i]:
+				new_container[i], new_container[i + 1] = new_container[i + 1], new_container[i]
+				no_change = False
+			print(f'step:{step} i:{i} rez:{no_change} Collection:{new_container}')
+		step += 1
+		print('-'*5)
+	print(f'\nResult v2: {new_container} \ntype:{type(container)}\n')
+	return new_container
+
+
+
+def sort_k1(container: Collection[_Tt], k) -> Collection[_Tt]:
+	"""
+	Поиск K минимумов в массиве (simple)
+	"""
+	rez = sorted(container)[0:k]
+	print(rez)
+	return rez
+
+
+def sort_k2(container: Collection[_Tt], k) -> Collection[_Tt]:
+	"""
+	Поиск K минимумов в массиве (любым из двух методов, без шаблона)
+	"""
+	i = 0
+	rez = container[0]
+	rez = range
+	print(rez)
+
+	for i in range(1, len(container) - 1):
+		if rez > container[i]:
+			rez = container[i]
+
+	print(rez)
+	return rez
+
+
+
+
+# a = [4, 5, 3, 2, 1]
+# a = frozenset(a)
+a = (3, 2, 1)
+# a = [4, 5, 3, 2, 1]
+# sort(a)
+# sort2(a)
+
+a = [4, 5, 6, 7, 5, 3, 2, 1, 5]
+# print(a, type(a), set(a), )
+sort_k1(a, 3)
+sort_k2(a, 3)
+
+
+
+# a = [4, 5, 3, 2, 1]
+# a1 = a[0]
+# a2 = if a[1]
+#
+# a1 =
+
+
+
