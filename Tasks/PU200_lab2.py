1713de7a578966820a10800820da6184afaa0f65
alepan789 <alpan82@list.ru>
2020-02-15 13:52:27 +0300

    PU200 - prakt 2

--- Tasks/PU200_lab2.py
+++ Tasks/PU200_lab2.py
@@ -1,4 +1,6 @@
 """
+    15.02.2020
+
     1 В Python нет возможности объявления константных переменных. Реализуйте
     с помощью @property константный атрибут. Пусть класс возвращает число
     пи. Попытайтесь применить @property к @staticmethod и @classmethod. Если
@@ -59,39 +61,99 @@ class Pi:
 #
 
 """ 
-    2. Создайте классы A и B(A). a. В классе А создайте атрибуты класса, атрибуты
+    2. Создайте классы A и B(A). 
+        a. В классе А создайте атрибуты класса, атрибуты 
     объекта, @staticmethod, @classmethod и методов со всеми видами областями
-    видимости. b. Продемонстрируете их видимость внутри класса, вне класса и в
-    классе потомке. c. Получите доступ вне класса к псевдозащищённым
+    видимости. 
+        b. Продемонстрируете их видимость внутри класса, вне класса и в
+    классе потомке. 
+        c. Получите доступ вне класса к псевдозащищённым
     псевдоприватным атрибутам и методам. (Тема 2. Слайды 1-43)
 
 """
 
 
 class A:
-    class_attribute = 'cls_attrib'
-    __class_attribute_2 = 'cls_attrib'
+    # это есть Атрибуты от КЛАССА !!!!!!!!!, не объекта (не экземпляра)
+    # НО это хорошо действует для изменяемых типов - например список и добавляем в него
+    # а если присваиваем "в лоб" можем словить разныве варианты - нужно учитывать
+    # если хотим менять атрибут класса, лучше обращаться через Класс, а не объект !!
+    class_attribute = [123]
+    __class_attribute_2 = ['cls_attrib']
 
-    def __init__(self, num):
+    def __init__(self, num, num2):
         self.attribute_1 = num + 1          # NORMAL
-        self._attribute_2 = num + 2         # protected
-        self.__attribute_3 = num + 3        # PRIVATE !!! - он переименовывается
+        self._attribute_1 = num + 2         # protected
+        self.__attribute_1 = num + 3        # PRIVATE !!! - он переименовывается c учетом класса
+        self.attribute_2 = num2 + 1  # NORMAL
 
 
-    def get_attrib_1(self):
-        return self.attribute_1
+    @staticmethod
+    def get_attrib_1():
+        # return 'qwer'
+        return A.class_attribute
 
+    @classmethod
+    def get_attrib_2(cls):
+        return cls.__class_attribute_2
 
 
 class B(A):
     ...
+    def __init__(self, num, num2):
+        # super().__init__(num, num2)
+        super().__init__()
+        self.attribute_b = num + 10          # NORMAL
+        self._attribute_b = num + 2         # protected
+        self.__attribute_b = num + 3        # PRIVATE !!! - он переименовывается
 
-varA = A()
-print(varA.class_attribute)
-varA.class_attribute = 'new value'
-print(varA.class_attribute)
 
-# интерпретатор приватные переменные переименовывает через имя класса, только так его можно вытащить
-print(varA._A__class_attribute_2)
-varA._A__class_attribute_2 = 'new value2'
-print(varA._A__class_attribute_2)
+
+
+varA = B(10, 3)
+varB = B(22, 3)
+
+# print(varA.class_attribute)
+# varA.class_attribute = 'new value'
+# print(varA.class_attribute)
+#
+# # интерпретатор приватные переменные переименовывает через имя класса, только так его можно вытащить
+# print(varA._A__class_attribute_2)
+# varA._A__class_attribute_2 = 'new value2'
+# print(varA._A__class_attribute_2)
+
+print(f'A id:{id(varA.class_attribute)}')
+print(f'B id:{id(varB.class_attribute)}')
+
+print(f'A:{varA.class_attribute}')
+print(f'B:{varB.class_attribute}')
+
+varA.class_attribute.append(73)
+print(f'A:{varA.class_attribute}')
+print(f'B:{varB.class_attribute}')
+
+varA.class_attribute = 13
+print(f'A:{varA.class_attribute}')
+print(f'B:{varB.class_attribute}')
+print(f'A dict:{varA.__dict__}')
+print(f'B dict:{varB.__dict__}')
+A.class_attribute = 55
+print(f'A:{varA.class_attribute}')
+print(f'B:{varB.class_attribute}')
+
+varC = A(11, 0)
+print(f'C:{varB.class_attribute}')
+
+print(f'A id:{id(varA.class_attribute)}')
+print(f'B id:{id(varB.class_attribute)}')
+print(f'C id:{id(varC.class_attribute)}')
+
+print(f'\tA get1:{varA.get_attrib_1()}')
+# print(f'A get2:{A.get_attrib_2}')
+
+var3 = B(17, 0)
+
+# print(f'A:{varA.class_attribute}')
+print(f'B dict:{var3.__dict__}, {var3}')
+print(f'B attrib from A; 1:{var3.attribute_1} 2:{var3.attribute_2} ')
+print(f'B dict:{var3.get_attrib_2()}')
